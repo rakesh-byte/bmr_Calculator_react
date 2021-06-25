40a3fec62599b7a6e5689165f732fd566aaf1427
Rakesh Kumar <rakesh.maheshwari040@gmail.com>
2021-06-25 16:16:09 +0500

    enviroment set

---
+++ src/components/bmr.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import './bmr.css'
+
+class Bmr extends Component {
+    render() {
+        return (
+            <div id="bmrcalc">
+                <div className="form">
+                    <h2>BMR &amp; Daily Calorie Calculator</h2>
+                    {/* Gender */}
+                    <div className="inputwrap">  
+                        <label className="label">Gender</label><label><input type="radio" className="genderF" name="gender" value="1" />Female</label><label><input type="radio" className="genderM" name="gender" value="2" />Male</label>
+                    </div>
+                   {/* Weight */}
+                    <div className="inputwrap">
+                        <label className="label">Weight in Pounds</label><input type="number" name="weight" className="weight" min="0" max="999" />
+                    </div>
+                    {/* Height */}
+                    <div className="inputwrap">
+                        <label className="label">Height in feet and inches</label><input type="number" name="heightFeet" className="heightFeet" min="0" max="8" /><input type="number" name="heightInches" className="heightInches" min="0" max="11" />
+                    </div>
+                   {/* Age */}
+                    <div className="inputwrap">
+                        <label className="label">Age in years</label><input type="number" className="age" name="age" min="0" max="120" />
+                    </div>
+                   {/* button to calculate BMR */}
+                    <button type="button">Calculate BMR</button>
+                   {/* workout list */}
+                    <div className="workout">
+                        <div className="inputwrap">
+                            <label className="label">Workout in a Week</label>
+                            <select className="activity" name="activity">
+                                <option value="">Select your Activity</option>
+                                <option value="1.2">Sedentary (Very little or no exercise, and desk job)</option>
+                                <option value="1.375">Lightly Active (Light exercise 1 to 3 days per week)</option>
+                                <option value="1.55">Moderately Active (Moderate exercise 3 to 5 days per week)</option>
+                                <option value="1.725">Very Active (Heavy exercise 6 to 7 days per week)</option>
+                                <option value="1.9">Extremely Active (Very intense exercise, and physical job, exercise multiple times per day)</option>
+                            </select>
+                        </div>
+                        {/* calories to be taken */}
+                        <button type="button">Calculate Calories</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Bmr;
